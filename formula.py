f56824058e4a77a925d04e598cb3644a204fea81
captainshinystar <joshie.dover@gmail.com>
2025-11-16 19:11:30 -0600

    updated formula.py

--- formula.py
+++ formula.py
@@ -4,20 +4,25 @@ class Formula:
     def __init__(self, name, ingredients):
         self.name = name
         self.ingredients = ingredients
+        self.invalid = []
+    
 
     def get_ingredients(self):
         return self.ingredients
     
     def check(self):
-        out_count = 0
-        out_of_stock = []
-        deact_count = 0
-        deactivated = []
         for ingredient in self.ingredients:
-            if ingredient.
+            if ingredient.status == Status.DEACTIVATED:
+                self.invalid.append(ingredient)
+            if ingredient.boh <= 0:
+                self.invalid.append(ingredient)
+        if self.invalid != []:
+            return True
+        else:
+            return False
 
     def update(self):
-        for ingredient in self.ingedients:
-            if ingredient.active == False:
-
-    
+        if self.check == True:
+            for ingredient in self.ingedients:
+                if ingredient.active == False:
+                    
